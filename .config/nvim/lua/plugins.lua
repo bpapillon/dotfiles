f7fb70b66b544269d7ce376b4fc0ef766d83511a
Ben Papillon <ben.papillon@relaypayments.com>
2022-10-04 22:05:13 -0400

    cleanup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -30,63 +30,6 @@ return require('packer').startup(function(use)
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
-  -- use({
-  --   "nvim-telescope/telescope.nvim",
-  --   requires = { "kyazdani42/nvim-web-devicons", "nvim-lua/plenary.nvim" }
-  --   config = function()
-  --     local actions = require("telescope.actions")
-
-  --     require("telescope").setup({
-  --       defaults = {
-  --         layout_config = {
-  --           prompt_position = "bottom",
-  --         },
-  --         layout_strategy = "bottom_pane",
-  --         path_display = {
-  --           shorten = {
-  --             len = 2,
-  --             exclude = { -1, -2 },
-  --           },
-  --         },
-  --       },
-  --       pickers = {
-  --         find_files = {
-  --           find_command = { "fd", "--hidden", "--glob", "", "--type", "file" },
-  --         },
-  --         live_grep = {
-  --           file_ignore_patterns = { "node_modules", ".git" },
-  --           find_command = "rg",
-  --           additional_args = function()
-  --             return {
-  --               "--no-heading",
-  --               "--with-filename",
-  --               "--line-number",
-  --               "--column",
-  --               "--hidden",
-  --               "--smart-case",
-  --             }
-  --           end,
-  --         },
-  --         buffers = {
-  --           mappings = {
-  --             i = {
-  --               ["<c-q>"] = actions.delete_buffer + actions.move_to_top,
-  --             },
-  --           },
-  --         },
-  --       },
-  --       extensions = {
-  --         ["ui-select"] = {
-  --           require("telescope.themes").get_dropdown({
-  --             layout_config = {
-  --               prompt_position = "top",
-  --             },
-  --           }),
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- })
 
   use('fatih/vim-go')
   use('leafgarland/typescript-vim')
