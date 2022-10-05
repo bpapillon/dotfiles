d8bcc0468e2338133e0013eb0d7ef6b4247b6347
Ben Papillon <ben.papillon@relaypayments.com>
2022-10-04 22:03:04 -0400

    improve new mac setup script

--- .bin/new_mac_setup.sh
+++ .bin/new_mac_setup.sh
@@ -1,8 +1,54 @@
 #!/bin/bash
 
-echo "Setting bash as default shell..."
-chsh -s /bin/bash
-source ~/.bashrc
+check_bin_install() {
+  COMMAND=${1}
+  echo "Checking for ${COMMAND} binary...\n"
+  if [ -x "$(which ${COMMAND})" ]; then
+    echo "INSTALLED\n"
+    return 0
+  else
+    echo "NOT FOUND\n"
+    return 1
+  fi
+}
+
+install_with_brew() {
+  BREW_PACKAGE=${1}
+  echo "Installing ${BREW_PACKAGE} with brew...\n"
+  brew install ${BREW_PACKAGE}
+  check_return_code $?
+}
+
+install_with_curl() {
+  CURL_PACKAGE=${1}
+  CURL_URL=${2}
+  echo "Installing ${CURL_PACKAGE} with curl from ${CURL_URL}...\n"
+  /bin/bash -c "$(curl -fsSL ${CURL_URL})"
+  check_return_code $?
+}
+
+install_with_npm() {
+  echo "Installing ${NPM_PACKAGE} with npm...\n"
+  npm install --global $NPM_PACKAGE
+}
+
+install_nvm() {
+  CURL_URL="https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh"
+  echo "Installing nvm with curl from ${CURL_URL}...\n"
+  curl -o- $CURL_URL | bash
+}
+
+install_rust() {
+  CURL_URL="https://sh.rustup.rs"
+  echo "Installing nvm with curl from ${CURL_URL}...\n"
+  curl –proto '=https' –tlsv1.2 -sSf $CURL_URL | sh
+}
+
+if [ "$SHELL" != "/bin/bash" ]; then
+  echo "Setting bash as default shell..."
+  chsh -s /bin/bash
+  source ~/.bashrc
+fi
 
 echo "Checking git and xcode installation..."
 git --version # Will prompt for xcode tools setup if needed
@@ -25,39 +71,33 @@ read -p "Public key has been copied to your clipboard. Add it to Github (https:/
 echo "Installing dotfiles..."
 curl -Ls https://raw.githubusercontent.com/bpapillon/dotfiles/master/.bin/install.sh | /bin/bash
 
-echo "Installing Homebrew..." # TODO check if homebrew already installed
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+check_bin_install "brew" || install_with_curl "brew" "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+brew update
 
-echo "Installing golang via Homebrew..."
-brew update && brew install golang
-echo "Installing psql via Homebrew..."
-brew install postgresql
-echo "Installing neovim via Homebrew..."
-brew install neovim
-echo "Installing pyenv via Homebrew..."
-brew install pyenv
-echo "Installing rbenv via Homebrew..."
-brew install rbenv
-echo "Installing hugo via Homebrew..."
-brew install hugo
-echo "Installing jq via Homebrew..."
-brew install jq
-echo "Installing awslogs via Homebrew..." # https://github.com/jorgebastida/awslogs
-brew install awslogs
-echo "Installing ripgrep via Homebrew..."
-brew install ripgrep
-echo "Installing fd via Homebrew..."
-brew install fd
+check_bin_install "go" || install_with_brew "go"
+check_bin_install "nvm" || install_nvm
+check_bin_install "rust" || install_rust
 
-echo "Installing nvm via curl-bash..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-echo "Installing yarn via npm..."
-npm install --global yarn
+check_bin_install "awslogs" || install_with_brew "awslogs"
+check_bin_install "fd" || install_with_brew "fd"
+check_bin_install "hugo" || install_with_brew "hugo"
+check_bin_install "jq" || install_with_brew "jq"
+check_bin_install "nvim" || install_with_brew "nvim"
+check_bin_install "psql" || install_with_brew "postgresql"
+check_bin_install "pyenv" || install_with_brew "pyenv"
+check_bin_install "rbenv" || install_with_brew "rbenv"
+check_bin_install "ripgrep" || install_with_brew "ripgrep"
+check_bin_install "yarn" || install_with_npm "yarn"
 
 echo "Setting up notes and projects directories..."
-mkdir ~/projects
-git clone git@github.com:bpapillon/notes.git ~/notes/
+if [[ ! -f "~/projects" ]]
+then
+  mkdir ~/projects
+fi
+if [[ ! -f "~/notes" ]]
+then
+  git clone git@github.com:bpapillon/notes.git ~/notes/
+fi
 
 cat << EOF
 Download and install the following apps:
